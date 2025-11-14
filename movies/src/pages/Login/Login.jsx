2a11864cdc0326d6d63d6d92a4e0905c25c49152
AnandKulkarnii <jodddd.jodddd@gmail.com>
2025-11-14 11:23:13 +0530

    bug fixed

--- movies/src/pages/Login/Login.jsx
+++ movies/src/pages/Login/Login.jsx
@@ -6,79 +6,82 @@ function Login() {
 
   const [password, setPassword] = useState("");
 
-  const onLogin = async() => {
+  const onLogin = async () => {
     if (email.length == 0) {
-      toast.warning('please enter email')
+      toast.warning("please enter email");
     } else if (password.length == 0) {
-      toast.warning('please enter password')
+      toast.warning("please enter password");
     } else {
-      const url = "http://localhost:4000/user/login"
-      const body = {email, password}
-      const res = await axios.post(url, body)
-      if (res.data['status'] == 'success') {
-        toast.success('Login successful')
-        localStorage.setItem('token', res.data['data']['token'])
+      const url = "http://localhost:4000/user/login";
+      const body = { email, password };
+      const res = await axios.post(url, body);
+      if (res.data["status"] == "success") {
+        toast.success("Login successful");
+        localStorage.setItem("token", res.data["data"]["token"]);
         setUser({
-          firstName: res.data['data']['firstName'],
-          lastName: res.data['data']['lastName'],
-        })
+          firstName: res.data["data"]["firstName"],
+          lastName: res.data["data"]["lastName"],
+        });
 
-        navigate('/home')
+        navigate("/home");
       } else {
-        toast.error(res.data['error'])
+        toast.error(res.data["error"]);
       }
     }
 
-  return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100">
-      <div
-        className="card p-4 shadow-lg"
-        style={{ maxWidth: "400px", width: "100%" }}
-      >
-        <div className="card-body">
-          <h3 className="card-title text-center mb-4">Login</h3>
+    return (
+      <div className="d-flex justify-content-center align-items-center min-vh-100">
+        <div
+          className="card p-4 shadow-lg"
+          style={{ maxWidth: "400px", width: "100%" }}
+        >
+          <div className="card-body">
+            <h3 className="card-title text-center mb-4">Login</h3>
 
-          <form onSubmit={handleSubmit}>
-            <div className="form-floating mb-3">
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                className="form-control"
-                id="floatingInput"
-                placeholder="name@example.com"
-                value={email}
-                required
-              />
-              <label htmlFor="floatingInput">Email address</label>
+            <form onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  className="form-control"
+                  id="floatingInput"
+                  placeholder="name@example.com"
+                  value={email}
+                  required
+                />
+                <label htmlFor="floatingInput">Email address</label>
+              </div>
+
+              <div className="form-floating mb-3">
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  className="form-control"
+                  id="floatingPassword"
+                  placeholder="Password"
+                  value={password}
+                  required
+                />
+                <label htmlFor="floatingPassword">Password</label>
+              </div>
+
+              <div className="mt-4">
+                <button
+                  onClick={() => onLogin()}
+                  className="btn btn-success w-100"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+
+            <div className="mt-3 text-center">
+              Don't have an account? <a href="/signup">SignUp</a>
             </div>
-
-            <div className="form-floating mb-3">
-              <input
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                className="form-control"
-                id="floatingPassword"
-                placeholder="Password"
-                value={password}
-                required
-              />
-              <label htmlFor="floatingPassword">Password</label>
-            </div>
-
-            <div className="mt-4">
-              <button onClick={()=>onLogin()} className="btn btn-success w-100">
-                Login
-              </button>
-            </div>
-          </form>
-
-          <div className="mt-3 text-center">
-            Don't have an account? <a href="/signup">SignUp</a>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 }
-
 export default Login;
