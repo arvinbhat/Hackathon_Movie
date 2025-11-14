e0c6cb04334c47a6f7f2d965ffb7f4aef9865fb4
Arvin Bhat <arvinbhat.ab@gmail.com>
2025-11-14 09:34:07 +0530

    updated sql queries

--- movies_backend/db.sql
+++ movies_backend/db.sql
@@ -1,74 +1,45 @@
--- database schema for airbnb application
-DROP DATABASE IF EXISTS airbnb_db;
-create database airbnb_db;
-use airbnb_db;
+DROP DATABASE IF EXISTS movies_db;
+create database movies_db;
+use movies_db;
 
--- user table
-create table user(
-    id integer primary key auto_increment,
+
+create table users(
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     firstName varchar(20),
     lastName varchar(20),
     email varchar(50),
     password varchar(100),
     phoneNumber varchar(15),
-    isDeleted integer(1) default 0, -- 0: not deleted, 1: deleted
+    isDeleted BOOLEAN default FALSE,
     createdTimestamp DATETIME default CURRENT_TIMESTAMP
 );
 
--- category table
 create table category (
-    id integer primary key auto_increment,
+    category_id int PRIMARY KEY AUTO_INCREMENT,
     title varchar(20),
     details varchar(1000),
-    image VARCHAR(200),
+    image varchar(200),
     createdTimestamp DATETIME default CURRENT_TIMESTAMP
 );
 
--- property table
-create table property (
-    id integer primary key auto_increment,
-    userId integer,
-    categoryId INTEGER,
-    title varchar(100),
-    details varchar(1000),
-    address varchar(1000),
-    contactNo VARCHAR(15),
-    ownerName VARCHAR(50),
-    isLakeView integer(1) default 0,
-    isTV integer(1) default 0,
-    isAC integer(1) default 0,
-    isWifi integer(1) default 0,
-    isMiniBar integer(1) default 0,
-    isBreakfast integer(1) default 0,
-    isParking integer(1) default 0,
-    guests INTEGER(1),
-    bedrooms INTEGER(1),
-    beds INTEGER(1),
-    bathrooms INTEGER(1),
-    rent FLOAT,
-
-    profileImage VARCHAR(100),
-    createdTimestamp DATETIME default CURRENT_TIMESTAMP
+create table movies (
+    movie_id INT PRIMARY KEY AUTO_INCREMENT,
+    movie_title varchar(100),
+    category_id int,
+    movie_details varchar(1000),
+    movie_image varchar(100),
+    createdTimestamp DATETIME default CURRENT_TIMESTAMP,
+    constraint fk_cat_id foreign key(category_id) references category(category_id)
 );
 
--- review table
-create table reviews (
-    id integer primary key auto_increment,
-    userId INTEGER,
-    propertyId INTEGER,
-    review VARCHAR(1000),
-    rating FLOAT,
-    image VARCHAR(100),
-    createdTimestamp DATETIME default CURRENT_TIMESTAMP
-);
 
--- bookings table
-create table bookings (
-    id integer primary key auto_increment,
-    userId integer,
-    propertyId integer,
-    fromDate varchar(50),
-    toDate varchar(50),
-    total float,
-    createdTimestamp DATETIME default CURRENT_TIMESTAMP
+create table reviews(
+    review_id int PRIMARY KEY AUTO_INCREMENT,
+    movie_id int,
+    review varchar(500),
+    rating int,
+    user_id int,
+    modified DATETIME default CURRENT_TIMESTAMP,
+    constraint fk_user_id FOREIGN KEY (user_id) references users(user_id),
+    constraint fk_movie_id FOREIGN KEY (movie_id) references movies(movie_id)
 );
